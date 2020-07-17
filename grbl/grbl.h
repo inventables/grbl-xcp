2182993358c3b6b4cc663a3547175de904dd5999
Neil Pankey <neil@inventables.com>
2020-07-17 09:35:44 -0700

    Stay aligned w/ Grbl versioning scheme

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -22,8 +22,8 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1h - XCP Proto 1.0.0"
-#define GRBL_VERSION_BUILD "20200707"
+#define GRBL_VERSION "1.1h-XCP"
+#define GRBL_VERSION_BUILD "20200717"
 
 #define X_CARVE_PRO
 
